e36346d8aee23cea7aec80b89b5634143be0b0f3
AlexeyParshin <dip1771@gmail.com>
2018-01-16 09:55:46 +0300

    part1

--- src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -1,13 +1,14 @@
 package lambda.part1.exercise;
 
-import com.google.common.collect.ImmutableList;
-import data.Person;
-import org.junit.Test;
-
-import java.util.List;
-
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import data.Person;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
 
 public class Lambdas02Exercise {
     @Test
@@ -19,6 +20,8 @@ public class Lambdas02Exercise {
         };
 
         // TODO use Arrays.sort
+
+        Arrays.sort(persons, (person1,person2) -> person1.getAge() - person2.getAge());
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
@@ -35,7 +38,8 @@ public class Lambdas02Exercise {
                 new Person("name 2", "lastName 1", 30)
         );
 
-        Person person = null;
+        Person person = FluentIterable.from(persons)
+                        .firstMatch(p -> p.getAge() == 30).get();
 
         // TODO use FluentIterable
 
