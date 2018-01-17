66832635a9e8925db9acfab5b6c92f30394036f7
Alina_Golovanova <alina_golovanova@epam.com>
2018-01-17 16:37:00 +0300

    task1

--- src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
@@ -1,18 +1,20 @@
 package lambda.part1.exercise;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-public class Lambdas02Exercise {
+public class Lambdas01Exercise {
 
     @Test
     public void sortPersonsByAge() {
@@ -22,7 +24,13 @@ public class Lambdas02Exercise {
                 new Person("name 2", "lastName 1", 30)
         };
 
-        Arrays.sort(persons, (p1, p2) -> p1.getAge() - p2.getAge());
+        Arrays.sort(persons, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return
+                        o1.getAge() - o2.getAge();
+            }
+        });
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
@@ -43,7 +51,12 @@ public class Lambdas02Exercise {
 
         final Optional<Person> personOptional =
                 FluentIterable.from(persons)
-                        .firstMatch(p -> p.getAge() == 30);
+                        .firstMatch(new Predicate<Person>() {
+                            @Override
+                            public boolean apply(Person person) {
+                                return person.getAge() == 30;
+                            }
+                        });
 
         if (personOptional.isPresent()) {
             person = personOptional.get();
