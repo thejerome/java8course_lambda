9af655ca1a9fc7fb9af266a762a369d6b4446312
Aleksei_Parshin <Aleksei_Parshin@epam.com>
2018-01-17 12:36:11 +0300

    Revert "part1"
    This reverts commit e36346d

--- src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -1,14 +1,13 @@
 package lambda.part1.exercise;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import data.Person;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class Lambdas02Exercise {
     @Test
@@ -20,8 +19,6 @@ public class Lambdas02Exercise {
         };
 
         // TODO use Arrays.sort
-
-        Arrays.sort(persons, (person1,person2) -> person1.getAge() - person2.getAge());
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
@@ -38,8 +35,7 @@ public class Lambdas02Exercise {
                 new Person("name 2", "lastName 1", 30)
         );
 
-        Person person = FluentIterable.from(persons)
-                        .firstMatch(p -> p.getAge() == 30).get();
+        Person person = null;
 
         // TODO use FluentIterable
 
